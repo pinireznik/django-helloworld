9730e2d87e0766f2bfd47cd21d22f8488067d0f9
Pini Reznik <p.reznik@uglyduckling.nl>
2014-03-31 14:39:14 +0200

    add start script

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@
 # cmd ["python manage.py", "runserver 0.0.0.0:8000"]
 
 
-# docker run -name django -v `pwd`:/home/docker/code/app/  -w /home/docker/code/app/ -p 8000:8000 -i -t dockerfiles/django-uwsgi-nginx bash
+# docker run -name django -v `pwd`:/home/docker/code/app/  -w /home/docker/code/app/ -p 8000:8000 -i -t dockerfiles/django-uwsgi-nginx ./startup.sh
 
 # python manage.py syncdb
 python manage.py runserver 0.0.0.0:8000
